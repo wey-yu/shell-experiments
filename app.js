4dafa28c5b91981d7346eaab87b6ee4971ebf4ec
Philippe Charrière <ph.charriere@gmail.com>
2017-01-01 09:23:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser");
 let port = process.env.PORT || 6666;
 
 let cmd = [
-  , `sudo apt-get install openjdk-8-jdk;`
+  , `apt-get install openjdk-8-jdk;`
   , `java -version;`
   , `ls; `
   , `cd mystore;`
