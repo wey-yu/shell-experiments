705aff5c0a634f9f1797a05a2a7665951ed912bc
ph.charriere <ph.charriere@gmail.com>
2017-01-01 08:16:27 +0100

    :rocket: 1st bidouille

---
+++ app.js
@@ -0,0 +1,43 @@
+// shell experiments
+"use strict";
+
+require('shelljs/global');
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+let port = process.env.PORT || 6666;
+
+let cmd = [
+  , `java -version; `
+  , `ls; `
+  , `pwd`
+].join('');
+
+exec(cmd, (code, stdout, stderr) => {
+  if(code !== 0) {
+    console.error(`😡 👎`);
+    console.error(stderr)
+
+  } else {
+    console.info('😀 👍')
+    console.info(stdout)
+  }
+});
+
+let app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => {
+  res.send({message:"💙", remark:"hello 🌍"});
+})
+
+app.get('/hi', (req, res) => {
+  res.send({message:"🐼", remark:"hi 🌍"});
+})
+
+app.listen(port)
+console.log(`🌍 Web Server is started - listening on ${port}`)
